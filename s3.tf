29335996bc578f9fe1a03a868037b0b8cc7bf613
nysdin <40791830+nysdin@users.noreply.github.com>
2023-10-01 00:41:08 +0900

    fluentbit設定ファイル

--- s3.tf
+++ s3.tf
@@ -1,3 +1,28 @@
 resource "aws_s3_bucket" "redash_fargate_artifact" {
   bucket = "redash-fargate-artifact"
 }
+
+resource "aws_s3_bucket" "nysdin_fluentbit_config" {
+  bucket = "nysdin-aws-for-fluent-bit-config"
+}
+
+resource "aws_s3_object" "nysdin_fluentbit_log_destinations_config" {
+  bucket = aws_s3_bucket.nysdin_fluentbit_config.bucket
+  key    = "outputs.conf"
+  source = "templates/aws-for-fluent-bit/outputs.conf"
+  etag   = filemd5("templates/aws-for-fluent-bit/outputs.conf")
+}
+
+resource "aws_s3_object" "nysdin_fluentbit_parsers_config" {
+  bucket = aws_s3_bucket.nysdin_fluentbit_config.bucket
+  key    = "parsers.conf"
+  source = "templates/aws-for-fluent-bit/parsers.conf"
+  etag   = filemd5("templates/aws-for-fluent-bit/parsers.conf")
+}
+
+resource "aws_s3_object" "nysdin_fluentbit_filters_config" {
+  bucket = aws_s3_bucket.nysdin_fluentbit_config.bucket
+  key    = "filters.conf"
+  source = "templates/aws-for-fluent-bit/filters.conf"
+  etag   = filemd5("templates/aws-for-fluent-bit/filters.conf")
+}
