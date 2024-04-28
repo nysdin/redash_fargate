e4778a7043ef0b689529a35ec210e5138265f098
nysdin <40791830+nysdin@users.noreply.github.com>
2024-04-28 18:06:31 +0900

    printenv

--- aws/scripts/application_start.sh
+++ aws/scripts/application_start.sh
@@ -7,8 +7,5 @@ echo "=============="
 cd /opt/redash_fargate
 # export IMAGE_TAG=${BUNDLE_COMMIT}
 echo $IMAGE_TAG
-if [[ "$DEPLOYMENT_GROUP_NAME" == "Staging" ]]
-then
-  export ENV=stgpo
-    sed -i -e 's/Listen 80/Listen 9090/g' /etc/httpd/conf/httpd.conf
-fi
+
+printenv
