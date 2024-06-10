980039f651b142d072200392adf2c40cefc47d3b
nysdin <40791830+nysdin@users.noreply.github.com>
2024-06-10 18:11:14 +0900

    set default region

--- aws/scripts/application_start.sh
+++ aws/scripts/application_start.sh
@@ -4,13 +4,15 @@ echo "=============="
 echo "ApplicationStart"
 echo "=============="
 
+export AWS_DEFAULT_REGION=ap-northeast-1
+
 aws s3 sync s3://test-hori-bpa /opt/assets
 
 TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 300")
 INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: ${TOKEN}" http://169.254.169.254/latest/meta-data/instance-id)
 
 # s3 sync といったデータロード等の初期化処理が終わった後に、RunCommand の対象となるタグを付与する
-aws ec2 --region ap-northeast-1 create-tags --resources "${INSTANCE_ID}" --tags Key=State,Value=Running
+aws ec2 create-tags --resources "${INSTANCE_ID}" --tags Key=State,Value=Running
 
 # wait for the attached tag to be reflected (on managed instance)
 while ! aws ssm describe-instance-information --filters Key=tag:ServiceType,Values=Batch Key=tag:State,Values=Running --query "InstanceInformationList[].[InstanceId]" --output text | grep "${INSTANCE_ID}"
--- graceful_shutdown.sh
+++ graceful_shutdown.sh
@@ -6,12 +6,13 @@ INTERVAL=30
 TIMEOUT=${GRACEFUL_SHUTDOWN_TIMEOUT:-3600}
 TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 300")
 INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: ${TOKEN}" http://169.254.169.254/latest/meta-data/instance-id)
+export AWS_DEFAULT_REGION=ap-northeast-1
 
 # Function to check if there are in-progress RunCommand invocations
 function exists_in_progress_run_command() {
   local instance_id=$1
 
-  if [[ $(aws --region ap-northeast-1 ssm list-command-invocations --instance-id "${instance_id}" --filters key=Status,value=InProgress | jq -r '.CommandInvocations | length') -gt 0 ]]; then
+  if [[ $(aws ssm list-command-invocations --instance-id "${instance_id}" --filters key=Status,value=InProgress | jq -r '.CommandInvocations | length') -gt 0 ]]; then
     return 0
   else
     return 1
@@ -19,10 +20,10 @@ function exists_in_progress_run_command() {
 }
 
 echo "Stopping acceptance of new job requests..."
-aws --region ap-northeast-1 ec2 create-tags --resources "${INSTANCE_ID}" --tags Key=State,Value=Terminating
+aws ec2 create-tags --resources "${INSTANCE_ID}" --tags Key=State,Value=Terminating
 
 # Wait for the attached tag to be reflected
-while ! aws --region ap-northeast-1 ssm describe-instance-information --filters Key=tag:ServiceType,Values=Batch Key=tag:State,Values=Terminating --query "InstanceInformationList[].[InstanceId]" --output text | grep "${INSTANCE_ID}"
+while ! aws ssm describe-instance-information --filters Key=tag:ServiceType,Values=Batch Key=tag:State,Values=Terminating --query "InstanceInformationList[].[InstanceId]" --output text | grep "${INSTANCE_ID}"
 do
   echo "Waiting for the attached tag to be reflected..."
   sleep 30
@@ -43,10 +44,10 @@ done
 if exists_in_progress_run_command "${INSTANCE_ID}"; then
   echo "RunCommand on instance(${INSTANCE_ID}) is still running. But Timeout..."
   echo "Below is the list of RunCommand with InProgress Status"
-  aws --region ap-northeast-1 ssm list-command-invocations --instance-id "${INSTANCE_ID}" --filters key=Status,value=InProgress | jq -r '.CommandInvocations[] | "CommandID: \(.CommandId), Status: \(.Status)"'
+  aws ssm list-command-invocations --instance-id "${INSTANCE_ID}" --filters key=Status,value=InProgress | jq -r '.CommandInvocations[] | "CommandID: \(.CommandId), Status: \(.Status)"'
 else
   echo "All RunCommand on instance($INSTANCE_ID) is completed!!!"
 fi
 
 # Complete the lifecycle action
-aws --region ap-northeast-1 autoscaling complete-lifecycle-action --lifecycle-hook-name "${LIFECYCLE_HOOK_NAME}" --auto-scaling-group-name "${AUTO_SCALING_GROUP_NAME}" --instance-id "${INSTANCE_ID}" --lifecycle-action-result CONTINUE
+aws autoscaling complete-lifecycle-action --lifecycle-hook-name "${LIFECYCLE_HOOK_NAME}" --auto-scaling-group-name "${AUTO_SCALING_GROUP_NAME}" --instance-id "${INSTANCE_ID}" --lifecycle-action-result CONTINUE
