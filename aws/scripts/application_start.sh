3a321b738fb4855c5aa0ca1e6f4144c86007ed88
nysdin <40791830+nysdin@users.noreply.github.com>
2024-06-10 10:55:21 +0900

    fix tag

--- aws/scripts/application_start.sh
+++ aws/scripts/application_start.sh
@@ -8,7 +8,7 @@ aws s3 sync s3://test-hori-bpa /opt/assets
 
 TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 300")
 INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: ${TOKEN}" http://169.254.169.254/latest/meta-data/instance-id)
-aws ec2 --region ap-northeast-1 create-tags --resources "${INSTANCE_ID}" --tags Key=Running,Value=true
+aws ec2 --region ap-northeast-1 create-tags --resources "${INSTANCE_ID}" --tags Key=State,Value=Running
 
 # cd /opt/redash_fargate
 # # export IMAGE_TAG=${BUNDLE_COMMIT}
