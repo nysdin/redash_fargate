c428ebe5925d7788c4408c33c2bc1c6e84954255
nysdin <40791830+nysdin@users.noreply.github.com>
2024-06-10 20:34:44 +0900

    timeoutはasg hookのheartbeat timeoutに任せる

--- graceful_shutdown.sh
+++ graceful_shutdown.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/bash -x
 
 AUTO_SCALING_GROUP_NAME=${1:-"yanada-test"}
 LIFECYCLE_HOOK_NAME=${2:-"graceful-termination"}
-INTERVAL=30
-TIMEOUT=${GRACEFUL_SHUTDOWN_TIMEOUT:-3600}
+INTERVAL=${INTERVAL:-30}
+
 TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 300")
 INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: ${TOKEN}" http://169.254.169.254/latest/meta-data/instance-id)
 export AWS_DEFAULT_REGION=ap-northeast-1
@@ -32,23 +32,12 @@ done
 echo "Stopped accepting new job requests."
 
 # Wait for the instance to complete InProgress RunCommands
-for ((i = 0; i < TIMEOUT / INTERVAL; i++)); do
-  if exists_in_progress_run_command "${INSTANCE_ID}"; then
-    echo "waiting for existing RunCommand to complete..."
-    sleep $INTERVAL
-  else
-    break
-  fi
-done
-
-# Display the list of RunCommand with InProgress Status (may be killed by timeout)
-if exists_in_progress_run_command "${INSTANCE_ID}"; then
-  echo "RunCommand on instance(${INSTANCE_ID}) is still running. But Timeout..."
-  echo "Below is the list of RunCommand with InProgress Status"
+while exists_in_progress_run_command "${INSTANCE_ID}"
+do
+  echo "waiting for existing RunCommand to complete..."
   aws ssm list-command-invocations --instance-id "${INSTANCE_ID}" --filters key=Status,value=InProgress | jq -r '.CommandInvocations[] | "CommandID: \(.CommandId), Status: \(.Status)"'
-else
-  echo "All RunCommand on instance($INSTANCE_ID) is completed!!!"
-fi
+  sleep "${INTERVAL}"
+done
 
 # Complete the lifecycle action
 aws autoscaling complete-lifecycle-action --lifecycle-hook-name "${LIFECYCLE_HOOK_NAME}" --auto-scaling-group-name "${AUTO_SCALING_GROUP_NAME}" --instance-id "${INSTANCE_ID}" --lifecycle-action-result CONTINUE
