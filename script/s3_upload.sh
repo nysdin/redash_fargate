92f53b70ad599189b0ed4e2f234d575fc30bdc06
nysdin <40791830+nysdin@users.noreply.github.com>
2024-09-03 13:35:49 +0900

    エラーも出力

--- script/s3_upload.sh
+++ script/s3_upload.sh
@@ -14,10 +14,10 @@ for dir in ${SYNC_DIRS}; do
     echo "\`\`\`" >> "$GITHUB_STEP_SUMMARY"
     for file in $(git diff "${DIFF_COMMIT}" --name-only --relative="${dir}" --diff-filter=d); do
       if [[ ${dir} == "contents" ]]; then s3_dir="teamsite"; else s3_dir="${dir}"; fi
-      aws s3 cp "${dir}/${file}" "s3://${BUCKET_NAME}/${s3_dir}/${file}"
+      aws s3 cp "${s3_dir}/${file}" "s3://${BUCKET_NAME}/${s3_dir}/${file}"
     done
     echo "\`\`\`"
-  } >> "$GITHUB_STEP_SUMMARY"
+  } >> "$GITHUB_STEP_SUMMARY" 2>&1
 
   # Delete Files
   {
@@ -27,5 +27,5 @@ for dir in ${SYNC_DIRS}; do
       aws s3 rm "s3://${BUCKET_NAME}/${dir}/${file}" >> "$GITHUB_STEP_SUMMARY" || true
     done
     echo "\`\`\`"
-  } >> "$GITHUB_STEP_SUMMARY"
+  } >> "$GITHUB_STEP_SUMMARY" 2>&1
 done
